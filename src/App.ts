f195db98a47865fa5dc5e4cc222484112dad97aa
Maria Bazhlekova <mbazhlek@gmail.com>
2018-12-01 16:02:49 -0600

    Get data from db

--- src/App.ts
+++ src/App.ts
@@ -1,22 +1,34 @@
 import * as express from 'express';
+import * as pg from 'pg';
 
 class App {
   public express;
+  public dbClient;
 
   constructor() {
     this.express = express();
+    this.dbClient = new pg.Client(process.env.DB_URL);
     this.mountRoutes();
   }
 
-  private mountRoutes(): void {
+  private mountRoutes = async () => {
     const router = express.Router();
-    router.get('/', (req, res) => {
-      res.json({
-        message: 'Hello World!',
+    router.get('/', async (req, res) => {
+      await this.dbClient.connect(async (err, client, done) => {
+        const results = [];
+        if (err) {
+          done();
+          console.log(err);
+          return res.status(500).json({ success: false, data: err });
+        }
+        const query = await client.query('SELECT * FROM numbers');
+        query.rows.forEach(row => results.push(row));
+        await client.end();
+        return res.json(results);
       });
     });
     this.express.use('/', router);
-  }
+  };
 }
 
 export default new App().express;
