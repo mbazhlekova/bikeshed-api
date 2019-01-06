e45b5656d1a52eb035a2dd6323a257f9560d42c4
Maria Bazhlekova <mbazhlek@gmail.com>
2019-01-06 08:42:55 -0600

    Explicitly define entities

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,8 +1,7 @@
 import 'reflect-metadata';
 import { createConnection, Connection, ConnectionOptions } from 'typeorm';
-import { join, dirname } from 'path';
-
-const baseDir = join(__dirname, '..');
+import Poll from '../entity/Poll';
+import Choice from '../entity/Choice';
 
 const connectionsOpts: ConnectionOptions = {
   type: 'postgres',
@@ -11,7 +10,7 @@ const connectionsOpts: ConnectionOptions = {
   username: process.env.DB_USERNAME || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
   database: process.env.DB_NAME || 'test',
-  entities: [`${baseDir}/**/*.entity.ts`],
+  entities: [Poll, Choice],
   synchronize: true,
 };
 
