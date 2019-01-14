6e67eb37ba336026b7ba5029983ea678b0035e57
Maria B <mbazhlek@gmail.com>
2019-01-13 19:52:37 -0600

    Add route to get all polls

---
+++ src/routes/poll.ts
@@ -0,0 +1,18 @@
+import { Request, Response, Router, NextFunction } from 'express';
+import { Poll } from '../model/Poll';
+
+class PollRoutes {
+  public static routes(): Router {
+    return Router().get('/polls', async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        const polls = await Poll.find();
+        res.send(polls);
+      } catch (error) {
+        res.status(500).json({ success: false, data: error });
+        next(error);
+      }
+    });
+  }
+}
+
+export default PollRoutes;
