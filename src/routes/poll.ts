68c24b16e9640ca0c684e439c46b0b460fe93ffc
Maria B <mbazhlek@gmail.com>
2019-01-14 21:28:20 -0600

    Add route to save poll

--- src/routes/poll.ts
+++ src/routes/poll.ts
@@ -3,15 +3,26 @@ import { Poll } from '../model/Poll';
 
 class PollRoutes {
   public static routes(): Router {
-    return Router().get('/polls', async (req: Request, res: Response, next: NextFunction) => {
-      try {
-        const polls = await Poll.find();
-        res.send(polls);
-      } catch (error) {
-        res.status(500).json({ success: false, data: error });
-        next(error);
-      }
-    });
+    return Router()
+      .get('/polls', async (req: Request, res: Response, next: NextFunction) => {
+        try {
+          const polls = await Poll.find();
+          res.send(polls);
+        } catch (error) {
+          res.status(500).json({ success: false, data: error });
+          next(error);
+        }
+      })
+      .post('/polls', async (req: Request, res: Response, next: NextFunction) => {
+        try {
+          const { poll } = req.body;
+          await Poll.create(poll);
+          res.send('Success! Poll saved');
+        } catch (error) {
+          res.status(500).json({ success: false, data: error });
+          next(error);
+        }
+      });
   }
 }
 
