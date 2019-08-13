017b47ba240b771737a4acce48f6fa34cc368295
Maria B <mbazhlek@gmail.com>
2019-08-13 06:21:59 -0500

    feature/add login route

---
+++ src/middleware/auth.ts
@@ -0,0 +1,18 @@
+import * as jwt from 'jsonwebtoken';
+import { Response, NextFunction } from 'express';
+import { RequestWithAuth } from '../types/Request';
+import { IUser } from '../model/User';
+
+export default (req: RequestWithAuth, res: Response, next: NextFunction) => {
+  const token = (req.headers['x-access-token'] as string) || req.headers[`authorization`];
+  if (!token) {
+    return res.status(401).send('Access denied. No token provided.');
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET);
+    req.user = decoded as IUser;
+    next();
+  } catch (ex) {
+    res.status(400).send('Invalid token.');
+  }
+};
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,27 +1,67 @@
-import { Request, Response, Router, NextFunction } from 'express';
-import * as jwt from 'jsonwebtoken';
+import { Request, Response, Router } from 'express';
 import * as bcrypt from 'bcryptjs';
-import { User } from '../model/User';
+import { User, validateUser } from '../model/User';
+import auth from '../middleware/auth';
+import { RequestWithAuth } from '../types/Request';
+import { NextFunction } from 'connect';
 
 class UserRoutes {
   public static routes(): Router {
-    return Router().post('/register', async (req: Request, res: Response, next: NextFunction) => {
-      try {
-        const { username, email, password } = req.body;
-        const hashedPassword = bcrypt.hashSync(password, 8);
-        const user = await User.create({
-          username,
-          email,
-          password: hashedPassword,
-        });
-        const token = jwt.sign({ id: user._id }, process.env.SECRET, {
-          expiresIn: 86400,
-        });
-        res.status(200).send({ auth: true, token });
-      } catch (error) {
-        res.status(500).send('There was a problem registering the user');
-      }
-    });
+    return Router()
+      .get('/current', auth, async (req: RequestWithAuth, res: Response) => {
+        const user = await User.findById(req.user._id).select('-password');
+        res.send(user);
+      })
+      .post('/register', async (req: Request, res: Response, next: NextFunction) => {
+        const { name, password, email } = req.body;
+        const { error } = validateUser(req.body);
+        if (error) {
+          return res.status(400).send(error.details[0].message);
+        }
+        try {
+          let user = await User.findOne({ email });
+          if (user) {
+            return res.status(400).send('User already registered.');
+          }
+
+          user = new User({
+            name,
+            password,
+            email,
+          });
+          user.password = await bcrypt.hash(user.password, 10);
+          await user.save();
+
+          const token = user.generateAuthToken();
+          res.header('x-auth-token', token).send({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+          });
+        } catch (error) {
+          res.status(500).send('There was a problem registering user');
+          next(error);
+        }
+      })
+      .post('/login', async (req: Request, res: Response, next: NextFunction) => {
+        const { email, password } = req.body;
+        try {
+          const user = await User.findOne({ email });
+          if (!user) {
+            return res.status(404).send('User with that email not found');
+          } else {
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) {
+              res.status(401).send({ auth: false, token: null });
+            }
+            const token = user.generateAuthToken();
+            res.send({ auth: true, token });
+          }
+        } catch (error) {
+          res.status(500).json({ success: false, data: error });
+          next(error);
+        }
+      });
   }
 }
 
